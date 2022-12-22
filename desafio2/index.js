8e63a041b75db874d927718a3db196fc89c9c393
unknown <you@example.com>
2022-12-21 23:25:56 -0500

    actualizando

--- desafio2/index.js
+++ desafio2/index.js
@@ -1,127 +1,129 @@
-const fs = require('fs')
+const fs = require("fs");
 
-class ProductManager{
-  constructor(path){
+class ProductManager {
+  constructor(path) {
     this.path = path;
     this.products = this.readFile();
   }
-  readFile(){
-    const data = JSON.parse(fs.readFileSync(`./${this.path}`,'utf-8'));
+  readFile() {
+    const data = JSON.parse(fs.readFileSync(`./${this.path}`, "utf-8"));
     return data;
   }
-  writeData(data){
-    console.log('entra');
+  writeData(data) {
     const dataStrings = JSON.stringify(data);
-    fs.writeFileSync(`./${this.path}`,dataStrings)
+    fs.writeFileSync(`./${this.path}`, dataStrings);
     return dataStrings;
-
   }
 
-  addProduct(product){
-    
-    if(this.products.find(element => element.code === product.code)){
-      console.log('entra al error');
-      throw new Error('ya existe')
-    } else if(!product.title || !product.description|| !product.price || !product.thumbanail || !product.code || !product.stock){
-      throw new Error('Los campos son  obligatorios')
+  addProduct(product) {
+    let saveProduct = this.products.find(
+      (element) => element.code === product.code
+    );
+    if (saveProduct) {
+      throw new Error("ya existe");
+    } else if (
+      !product.title ||
+      !product.description ||
+      !product.price ||
+      !product.thumbanail ||
+      !product.code ||
+      !product.stock
+    ) {
+      throw new Error("Los campos son  obligatorios");
     }
-    let data = this.readFile()
+    let data = this.readFile();
     data.push(product);
     this.writeData(data);
   }
-  getProduct(){
+  getProduct() {
     let data = this.readFile();
     return data;
   }
-  
-  getProductByID(id){
-    let data = this.readFile()
-    let save = data.find(element => element.id === id)
-    if(save){
-      console.log('entra al id');
+
+  getProductByID(id) {
+    let data = this.readFile();
+    let save = data.find((element) => element.id === id);
+    if (save) {
       console.log(save);
-     
-    } else{
-      throw new Error('not found')
+    } else {
+      throw new Error("not found");
+    }
+  }
 
+  deleteProduct(id) {
+    let data = this.readFile();
+    let products = data.find((item) => item.id === id);
+
+    if (products) {
+      let deleteProduct = data.filter((item) => item.id !== id);
+      this.writeData(deleteProduct);
+      console.log(deleteProduct);
     }
+    console.log("No se encuentra producto  para eliminar con  ese id");
   }
-  deleteProduct(id){
-    if(this.products.find( element => element.id === id)){
-      console.log( delete this.products[id]); 
-    }
-    console.log('No se encuentra producto  para eliminar con  ese id');
-  }
-  updateProduct(id, obj){
-    if(this.products.find(item => item.id === obj.id)){
-      const guardar = this.products[id]
-      for(let key in obj){
+
+  updateProduct(id, obj) {
+    
+    let data = this.readFile();
+    let saveData = data.find((item) => item.id === id);
+    if (saveData) { 
+      let guardar = saveData;
+      for (let key in obj) {
         guardar[key] = obj[key];
-        
+        data.push(guardar);
+        this.writeData(data);
       }
-      guardar.push(obj);
-      this.writeData(guardar);
-
+      console.log(guardar);
+    } else {
+      console.log("no se puede modificar");
     }
-
-
   }
-
-
-  
-  
-
-  
 }
 
-
-
-class Product{
-  static  countProduct = 0;
-  constructor(title, description, price, thumbanail, code, stock){
-    this.id = Product.countProduct++
+class Product {
+  static countProduct = 0;
+  constructor(title, description, price, thumbanail, code, stock) {
+    this.id = Product.countProduct++;
     this.title = title;
     this.description = description;
     this.price = price;
     this.thumbanail = thumbanail;
     this.code = code;
-    this.stock = stock
+    this.stock = stock;
   }
- 
 }
 
-let persona1 = new ProductManager('data.json');
-let producto0 = new Product('Pantalon', 'azul', 50000, 'img', 123, 10);
-let producto1 = new Product('camisa', ' negra', 20000, 'img', 124, 19);
-let producto2 = new Product('medias', 'blanca', 50000, 'img', 12, 8);
-let producto3 = new Product('zapato', 'blancas', 50000, 'img', 1,3);
+let persona1 = new ProductManager("data.json");
+let producto0 = new Product("Pantalon", "azul", 50000, "img", 123, 10);
+let producto1 = new Product("camisa", " negra", 20000, "img", 124, 19);
+let producto2 = new Product("medias", "blanca", 50000, "img", 12, 8);
+let producto3 = new Product("zapato", "blancas", 50000, "img", 1, 3);
 
+// 1
+// persona1.addProduct(producto1);
+// persona1.addProduct(producto2);
+// persona1.addProduct(producto3);
 
-persona1.addProduct(producto0);
+//2
+// error ya existe el code
+// persona1.addProduct(producto1);
 
-
-
-// error ya existe el code 
-// persona1.addProduct(producto1);        
-
+// 3
 // Mostrar productos
 // console.log(persona1.getProduct());
 
+//4
 // Buscar producto
-persona1.getProductByID(0)
+// persona1.getProductByID(3)
 
-// Eliminar producto 
-// persona1.deleteProduct(7)
+//5
+// Eliminar producto
+// persona1.deleteProduct(3)
 
-// actualizar id 
-// persona1.updateProduct(2,
-//   {id: 2,
-//   title: 'actualizes',
-//   description: 'blancaghh',
-//   }
-  
-
-// )
-
-  
-  
+//6
+// actualizar id
+// persona1.updateProduct(1,
+//   {
+//   title: 'sombrero',
+//   description: 'rojo',
+//   })
